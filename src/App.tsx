f502dee6995cf3fb5dc1f4f07657c3f3f6c70c62
mohit <mohitkumravat22@gmail.com>
2025-11-19 09:52:21 -0800

    update css

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,15 @@ import LoginPage from "../src/api/components/pages/LoginPage.tsx";
 import SignupPage from "../src/api/components/pages/SignupPage.tsx";
 import PublicationsPage from "../src/api/components/pages/PublicationsPage.tsx";
 import EditPublicationPage from "../src/api/components/pages/EditPublicationPage.tsx";
-// import PublicFeedPage from "../src/api/components/pages";
 
 export default function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          {/* Public pages */}
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
 
-          {/* Public view (no login required) */}
-          {/* <Route path="/public" element={<PublicFeedPage />} /> */}
-
-          {/* Protected pages */}
           <Route
             path="/publications"
             element={
@@ -48,7 +42,6 @@ export default function App() {
             }
           />
 
-          {/* Default route */}
           <Route path="/" element={<Navigate to="/publications" />} />
         </Routes>
       </BrowserRouter>
