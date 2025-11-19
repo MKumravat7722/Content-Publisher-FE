4e38ce95045308760bbe10997e0af01453a11f2d
mohit <mohitkumravat22@gmail.com>
2025-11-19 10:14:48 -0800

    fix FE issue

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
 });
-
+// https://content-publisher-fe.onrender.com
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
